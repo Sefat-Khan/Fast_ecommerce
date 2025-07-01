38e775e79de2df431f2d4f2e19993ff6d6adeb8b
Sefat khan <sefatkhan144@gmail.com>
2025-07-01 22:14:38 +0600

    es

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,21 @@
+import { FlatCompat } from "@eslint/eslintrc";
+import nextEslint from "next/core-web-vitals";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  recommendedConfig: {
+    js: nextEslint,
+  },
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
-
-export default eslintConfig;
+export default [
+  nextEslint,
+  ...compat.config({
+    extends: ["next/core-web-vitals"],
+  }),
+];
