b224d549a4f6a03dacd348c61f1410effd50c6f9
Sefat khan <sefatkhan144@gmail.com>
2025-07-01 20:32:09 +0600

    jj

--- config/inngest.ts
+++ config/inngest.ts
@@ -8,24 +8,27 @@ export const inngest = new Inngest({ id: "fastcart-next" });
 // Inngest Functions to save user data
 
 export const syncUserCreation = inngest.createFunction(
-  {
-    id: "sync-user-from-clerk",
-  },
-  {
-    event: "clerk/user.created",
-  },
-  async ({ event }) => {
-    const { id, first_name, last_name, email_addresses, image_url } =
-      event.data;
-    const useData = {
-      _id: id,
-      name: first_name + " " + last_name,
-      email: email_addresses[0].email_address,
-      image: image_url || "",
-    };
-    // Save user data to your database
-    await connectDB();
-    await User.create(useData);
+  { id: "sync-user-from-clerk" },
+  { event: "clerk/user.created" },
+  async ({ event, logger }) => {
+    // `logger` is provided by Inngest
+    try {
+      const { id, first_name, last_name, email_addresses, image_url } =
+        event.data;
+      const userData = {
+        _id: id,
+        name: `${first_name} ${last_name}`,
+        email: email_addresses[0].email_address,
+        image: image_url || "",
+      };
+
+      await connectDB();
+      await User.create(userData);
+      logger.info(`User ${id} created successfully`);
+    } catch (error) {
+      logger.error("Failed to create user:", error);
+      throw error; // Re-throw to mark the function as failed
+    }
   }
 );
 
@@ -38,18 +41,25 @@ export const syncUserUpdate = inngest.createFunction(
   {
     event: "clerk/user.updated",
   },
-  async ({ event }) => {
-    const { id, first_name, last_name, email_addresses, image_url } =
-      event.data;
-    const useData = {
-      _id: id,
-      name: first_name + " " + last_name,
-      email: email_addresses[0].email_address,
-      image: image_url || "",
-    };
-    // Save updated user data to your database
-    await connectDB();
-    await User.findByIdAndUpdate(id, useData);
+  async ({ event, logger }) => {
+    // `logger` is provided by Inngest
+    try {
+      const { id, first_name, last_name, email_addresses, image_url } =
+        event.data;
+      const userData = {
+        _id: id,
+        name: `${first_name} ${last_name}`,
+        email: email_addresses[0].email_address,
+        image: image_url || "",
+      };
+
+      await connectDB();
+      await User.findByIdAndUpdate(id, userData);
+      logger.info(`User ${id} updated successfully`);
+    } catch (error) {
+      logger.error("Failed to update user:", error);
+      throw error; // Re-throw to mark the function as failed
+    }
   }
 );
 
@@ -62,10 +72,16 @@ export const syncUserDelete = inngest.createFunction(
   {
     event: "clerk/user.deleted",
   },
-  async ({ event }) => {
-    const { id } = event.data;
-    // Save updated user data to your database
-    await connectDB();
-    await User.findByIdAndDelete(id);
+  async ({ event, logger }) => {
+    try {
+      const { id } = event.data;
+      // Save updated user data to your database
+      await connectDB();
+      await User.findByIdAndDelete(id);
+      logger.info(`User ${id} deleted successfully`);
+    } catch (error) {
+      logger.error("Failed to delete user:", error);
+      throw error; // Re-throw to mark the function as failed
+    }
   }
 );
