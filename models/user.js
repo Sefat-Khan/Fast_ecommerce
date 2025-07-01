41c62a73aa1e926aa00413e6feea5667a759aae1
Sefat khan <sefatkhan144@gmail.com>
2025-07-01 20:57:16 +0600

    z

--- models/user.js
+++ models/user.js
@@ -1,42 +1,29 @@
 import mongoose from "mongoose";
 
-interface IUser extends mongoose.Document {
-  _id: string; // Changed from 'id' to '_id' to match your usage
-  name: string;
-  email: string;
-  imageUrl: string;
-  cartItems: Record<string, unknown>;
-}
+const userSchema = new mongoose.Schema({
+  id: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  imageUrl: {
+    type: String,
+    required: true,
+  },
+  cartItems: {
+    type: Object,
+    default: {},
+  },
+});
 
-const userSchema =
-  new mongoose.Schema() <
-  IUser >
-  {
-    _id: {
-      // Changed from 'id' to '_id'
-      type: String,
-      required: true,
-    },
-    name: {
-      type: String,
-      required: true,
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    imageUrl: {
-      type: String,
-      required: true,
-    },
-    cartItems: {
-      type: Object,
-      default: {},
-    },
-  };
-
-const User =
-  mongoose.models.User || mongoose.model < IUser > ("User", userSchema);
+const User = mongoose.models.user || mongoose.model("user", userSchema);
 
 export default User;
