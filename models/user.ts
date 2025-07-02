0e8fa505dae68d85729eea521c93bb259abacbb2
Sefat khan <sefatkhan144@gmail.com>
2025-07-02 10:02:09 +0600

    c

---
+++ models/user.ts
@@ -0,0 +1,27 @@
+import mongoose, { Document, Model } from "mongoose";
+
+interface IUser extends Document {
+  _id: string;
+  name: string;
+  email: string;
+  imageUrl: string;
+  cartItems: Record<string, unknown>;
+}
+
+const userSchema =
+  new mongoose.Schema() <
+  IUser >
+  {
+    _id: { type: String, required: true },
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    imageUrl: { type: String, required: true },
+    cartItems: { type: Object, default: {} },
+  };
+
+type UserModel = Model<IUser>;
+
+const User: UserModel =
+  mongoose.models.User || mongoose.model < IUser > ("User", userSchema);
+
+export default User;
