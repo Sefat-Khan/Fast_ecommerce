b6ca1cba2a0c96691af8828b3204cf453f5eee6d
Sefat khan <sefatkhan144@gmail.com>
2025-07-02 10:08:08 +0600

    n

--- models/user.ts
+++ models/user.ts
@@ -8,20 +8,17 @@ interface IUser extends Document {
   cartItems: Record<string, unknown>;
 }
 
-const userSchema =
-  new mongoose.Schema() <
-  IUser >
-  {
-    _id: { type: String, required: true },
-    name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    imageUrl: { type: String, required: true },
-    cartItems: { type: Object, default: {} },
-  };
+const userSchema = new mongoose.Schema<IUser>({
+  _id: { type: String, required: true },
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  imageUrl: { type: String, required: true },
+  cartItems: { type: Object, default: {} },
+});
 
 type UserModel = Model<IUser>;
 
 const User: UserModel =
-  mongoose.models.User || mongoose.model < IUser > ("User", userSchema);
+  mongoose.models.User || mongoose.model<IUser>("User", userSchema);
 
 export default User;
