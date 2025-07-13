8ba57bbf7a99a7b574d9c89ffb29a8105184dac1
Sefat khan <sefatkhan144@gmail.com>
2025-07-13 14:30:02 +0600

    fix

--- app/api/order/list/route.ts
+++ app/api/order/list/route.ts
@@ -1,9 +1,7 @@
 import { getAuth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 import { connectDB } from "../../../../config/db";
-import Address from "../../../../models/address";
 import Order from "../../../../models/order";
-import Product from "../../../../models/product";
 
 export async function GET(req) {
   try {
@@ -11,12 +9,7 @@ export async function GET(req) {
 
     await connectDB();
 
-    Address.length;
-    Product.length;
-
-    const orderData = await Order.find({ userId })
-      .populate("address")
-      .populate("items.product");
+    const orderData = await Order.find({ userId });
     return NextResponse.json({
       success: true,
       orderData,
